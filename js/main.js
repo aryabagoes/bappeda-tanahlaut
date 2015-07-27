43bd9781b59280881ec12ed5119cd0127d098de3
Muslih <muslihzarth@gmail.com>
2015-07-27 22:50:36 +0800

    Fix error responsive menu

--- js/main.js
+++ js/main.js
@@ -1,4 +1,63 @@
 // console.log("walhh");
+$(window).load(function(){
+    $('.flexslider').flexslider({
+        animation:"fade",
+        slideshowSpeed: 3500,
+        ControlsContainer:".flex-container"
+    });
+});
+
+
+$(document).ready(function(){
+ 
+    lebar = $(window).width();
+    mainmenu = $("#main-menu");
+    $("#resp-menu").click(function(){ 
+         mainmenu.slideToggle(500);
+    });
+
+});
+
+// SLIDER
+$(window).load(function(){
+    $('.flexslider').flexslider({
+        animation:"fade",
+        slideshowSpeed: 3500,
+        ControlsContainer:".flex-container"
+    });
+});
+
+// NAVIGASI FIX 
+// $(document).ready(function(){
+    
+//     var stickyNavTop = $('nav').offset().top;
+//     var stickyNav = function()
+
+//     	{
+//         	var scrollTop = $(window).scrollTop();
+
+// 	        if (scrollTop > stickyNavTop) {
+// 	            $('nav').addClass('fixed');
+// 	        } 
+// 	        else {		           
+// 	            $('nav').removeClass('fixed');
+// 	        }	         				
+//     	};
+
+//     // stickyNav();
+//     $(window).scroll(function() {
+//         stickyNav();
+//     });     
+// });
+
+// $(window).resize(function() {
+// var descTop = $(window).width()
+// 	if (descTop > 768){ 
+// 	// window.location.reload();
+// 	// document.getElementById("main-menu").style.display = "none";
+// 	}	
+// });
+
 $(document).ready(function()
 {
 	$('body .awan-latar')
